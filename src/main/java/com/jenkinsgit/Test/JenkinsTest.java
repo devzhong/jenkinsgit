ef894ee23af0e87a49558bc74527329dad67d976
devzhong <devzhong@163.com>
2019-04-30 10:39:42 +0800

    tt

--- src/main/java/com/jenkinsgit/Test/JenkinsTest.java
+++ src/main/java/com/jenkinsgit/Test/JenkinsTest.java
@@ -2,6 +2,10 @@ package com.jenkinsgit.Test;
 
 public class JenkinsTest {
 
+    public static void main(String[] args) {
+        say();
+    }
+
     public static void say(){
         System.out.println("Hello Jenkins&git");
     }
